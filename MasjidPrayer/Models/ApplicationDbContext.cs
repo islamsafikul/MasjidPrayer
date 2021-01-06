4141a76aaec4bb695f45178fa6f7baf63ef1701f
Safikul Islam <safikulislam.connect@gmail.com>
2021-01-06 10:09:18 +0530

    Add project files.

---
+++ MasjidPrayer/Models/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MasjidPrayer.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.HasDefaultSchema("EventRegistration");
+        }
+    }
+}
